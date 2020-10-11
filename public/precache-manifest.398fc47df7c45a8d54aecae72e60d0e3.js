91840f2f2328871d56befbdbb4cb63dde3bd1094
milkdue <321432896@qq.com>
2020-10-12 00:28:34 +0800

    this is system server

---
+++ public/precache-manifest.398fc47df7c45a8d54aecae72e60d0e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a91bc93f5395fef197e7781d236eab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1b2e4e261f5928a4926",
+    "url": "/static/css/2.342fb318.chunk.css"
+  },
+  {
+    "revision": "6669b9074390130a589a",
+    "url": "/static/css/main.89e6fc43.chunk.css"
+  },
+  {
+    "revision": "c1b2e4e261f5928a4926",
+    "url": "/static/js/2.d724b6f1.chunk.js"
+  },
+  {
+    "revision": "201796eeb0b0bcf35a2a8497f68ab254",
+    "url": "/static/js/2.d724b6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6669b9074390130a589a",
+    "url": "/static/js/main.156922ec.chunk.js"
+  },
+  {
+    "revision": "24e033b9ac9675f80a35",
+    "url": "/static/js/runtime-main.61474b88.js"
+  },
+  {
+    "revision": "4b02b891a8ad7d3755e9547390f743e8",
+    "url": "/static/media/bg.4b02b891.jpg"
+  },
+  {
+    "revision": "0e3f6e380f39005deae047795a33c9d7",
+    "url": "/static/media/logo.0e3f6e38.png"
+  }
+]);
